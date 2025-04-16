b89d441b1c9de14c28b42c234d9689d1991cc331
Pedro Henrique Ribeiro Moreira <p3drohenrique.moreira@gmail.com>
2025-04-16 07:51:07 -0300

    Ajuste na verificação Form2
    Correção na verificação lógica usando "OrElse" no operador ternário

--- Form2.vb
+++ Form2.vb
@@ -13,9 +13,9 @@
         lblTravelType.Text = _viagem.TipoViagem
         lblOperationPoint.Text = _viagem.PontoOperacional
         lblTruck.Text = _viagem.PlacaCavalo
-        lblTrailer1.Text = If(_viagem.PlacaCarreta1 Is Nothing Or _viagem.PlacaCarreta1.Length < 1, "Sem placa", _viagem.PlacaCarreta1)
+        lblTrailer1.Text = If(_viagem.PlacaCarreta1 Is Nothing OrElse _viagem.PlacaCarreta1.Length < 1, "Sem placa", _viagem.PlacaCarreta1)
         lblTrailer2.Text = If(_viagem.PlacaCarreta2, "Sem placa")
-        lblPlan.Text = If(_viagem.Plano Is Nothing Or _viagem.Plano.Length < 1, "Sem plano", _viagem.Plano)
+        lblPlan.Text = If(_viagem.Plano Is Nothing OrElse _viagem.Plano.Length < 1, "Sem plano", _viagem.Plano)
         lblStatus.Text = If(_viagem.Status, "Sem status")
         lblPhone.Text = If(_viagem.NumeroFone, "Sem número telefone")
 

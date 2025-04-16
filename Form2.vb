302e70107aed2a619e733fce08424f8705eb18ed
Pedro Henrique Ribeiro Moreira <p3drohenrique.moreira@gmail.com>
2025-04-16 07:54:58 -0300

    Ajuste na verificações usando o operador ternário

--- Form2.vb
+++ Form2.vb
@@ -14,10 +14,10 @@
         lblOperationPoint.Text = _viagem.PontoOperacional
         lblTruck.Text = _viagem.PlacaCavalo
         lblTrailer1.Text = If(_viagem.PlacaCarreta1 Is Nothing OrElse _viagem.PlacaCarreta1.Length < 1, "Sem placa", _viagem.PlacaCarreta1)
-        lblTrailer2.Text = If(_viagem.PlacaCarreta2, "Sem placa")
+        lblTrailer2.Text = If(_viagem.PlacaCarreta2 Is Nothing OrElse _viagem.PlacaCarreta2.Length < 1, "Sem placa", _viagem.PlacaCarreta2)
         lblPlan.Text = If(_viagem.Plano Is Nothing OrElse _viagem.Plano.Length < 1, "Sem plano", _viagem.Plano)
-        lblStatus.Text = If(_viagem.Status, "Sem status")
-        lblPhone.Text = If(_viagem.NumeroFone, "Sem número telefone")
+        lblStatus.Text = If(_viagem.Status Is Nothing OrElse _viagem.Status.Length < 1, "Sem status", _viagem.Status)
+        lblPhone.Text = If(_viagem.NumeroFone Is Nothing OrElse _viagem.NumeroFone.Length < 1, "Sem número telefone", _viagem.NumeroFone)
 
         dgvSuppliers.DataSource = _viagem.Fornecedores
         dgvPlants.DataSource = _viagem.Plantas
